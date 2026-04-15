40183125f6f769255423ab3c93d20874a27171eb
haitham71 <hri7isd@gmail.com>
2026-04-15 19:10:52 +0300

    sstf

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for converting values to key-value tuple."""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple with the string and the square of the number as float."""
+    return (k, float(v ** 2))
