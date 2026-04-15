5af35d619a2656e972c69e5d11fa063ee6a97d7e
haitham71 <hri7isd@gmail.com>
2026-04-15 19:12:00 +0300

    pystyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,5 +6,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Returns a tuple with the string and the square of the number as float."""
+    """Returns a tuple with the string"""
     return (k, float(v ** 2))
