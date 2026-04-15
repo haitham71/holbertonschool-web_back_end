86a03a78711cbe0b80cec13e587977b660f45d2b
haitham71 <hri7isd@gmail.com>
2026-04-15 18:45:28 +0300

    task2

---
+++ python_variable_annotations/1.add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for basic type-annotated functions."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the sum of two floats."""
+    return str1 + str2
