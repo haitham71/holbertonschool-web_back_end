2a689883d4f3a2cfd2686c4167c90061c755e4d2
haitham71 <hri7isd@gmail.com>
2026-04-28 19:21:05 +0300

    py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,11 +3,13 @@
 Simple helper function for pagination
 """
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     Returns a tuple of (start_index, end_index)
     corresponding to the pagination range.
     """
+    
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
