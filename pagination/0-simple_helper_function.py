467b377ab76760bc715961a575d3b20ed14fb4b2
haitham71 <hri7isd@gmail.com>
2026-04-28 19:21:45 +0300

    pystyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,7 +9,7 @@ def index_range(page: int, page_size: int) -> tuple:
     Returns a tuple of (start_index, end_index)
     corresponding to the pagination range.
     """
-    
+
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
